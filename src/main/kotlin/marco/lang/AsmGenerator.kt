4e0d55e6d9dee8b2a0d05070687a8a0671e0fcc7
marco <workmarco98@gmail.com>
2022-06-28 19:34:40 +0100

    Write text to file example.

--- src/main/kotlin/marco/lang/AsmGenerator.kt
+++ src/main/kotlin/marco/lang/AsmGenerator.kt
@@ -17,7 +17,8 @@ class AsmGenerator(private val tokens: List<Token>) {
             .append("\tsyscall\n")
         textSection.append("_start:\n")
 
-        for (token in tokens) {
+        for (i in tokens.indices) {
+            val token = tokens[i]
             when (token.type) {
                 TokenType.IDENTIFIER -> {
                     if (token.lex == "println") {
@@ -26,10 +27,29 @@ class AsmGenerator(private val tokens: List<Token>) {
                         textSection.append("\tmov rsi, str_").append(strCount).append("\n")
                         textSection.append("\tmov rdx, str_len_").append(strCount).append("\n")
                         textSection.append("\tsyscall\n")
+                    } else if(token.lex == "file") {
+                        // FIXME: 28/06/22 Currently the flags and umode are hardcoded!
+                        // FIXME: 28/06/22 abstract write syscall and make a system to store return values like the file descriptor
+                        if(tokens[i + 1].type == TokenType.STRING && tokens[i + 2].type == TokenType.STRING) {
+                            textSection.append("\tmov rax, 2\n")
+                            textSection.append("\tmov rdi, str_").append(strCount).append("\n")
+                            textSection.append("\tmov rsi, 0102o\n")
+                            textSection.append("\tmov rdx, 0666o\n")
+                            textSection.append("\tsyscall\n")
+                            textSection.append("\tmov rdi, rax\n")
+                            textSection.append("\tmov rax, 1\n")
+                            textSection.append("\tmov rsi, str_").append(strCount + 1).append("\n")
+                            textSection.append("\tmov rdx, str_len_").append(strCount + 1).append("\n")
+                            textSection.append("\tsyscall\n")
+                            textSection.append("\tmov rax, 3\n")
+                            textSection.append("\tsyscall\n")
+                        }
                     }
                 }
                 TokenType.STRING -> {
-                    dataSection.append("\tstr_").append(strCount).append(": db ").append(token.lex).append(", 10\n")
+                    dataSection.append("\tstr_").append(strCount).append(": db ").append(token.lex)
+                    if((token.extra as StringExtra).newLine) dataSection.append(", 10\n")
+                    else dataSection.append(", 0\n")
                     dataSection.append("\tstr_len_").append(strCount).append(" equ $-str_").append(strCount).append("\n")
                     strCount++
                 }
