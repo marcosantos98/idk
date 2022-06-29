b84ce8c6d3e1a1cc7e7cf485b6046376c93bb82f
marco <workmarco98@gmail.com>
2022-06-29 21:24:05 +0100

    Add new option and include AstBuilder.
    Also removed AsmGenerator for now.

--- src/main/kotlin/marco/main.kt
+++ src/main/kotlin/marco/main.kt
@@ -3,7 +3,7 @@ package marco
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.default
-import marco.lang.AsmGenerator
+import marco.lang.AstBuilder
 import marco.lang.Tokenizer
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -12,19 +12,28 @@ fun main(args: Array<String>) {
 
     val argParser = ArgParser("idk")
     val input by argParser.argument(ArgType.String, description = "Input source file")
-    val output by argParser.option(ArgType.String, shortName = "o", description = "Output asm file")
-    val printAsm by argParser.option(ArgType.Boolean, shortName = "p", description = "Print generated asm").default(false)
-
+    //val output by argParser.option(ArgType.String, shortName = "o", description = "Output asm file")
+    //val printAsm by argParser.option(ArgType.Boolean, shortName = "p", description = "Print generated asm").default(false)
+    val debug by argParser.option(ArgType.Boolean, shortName = "d", description = "Only prints the tokens")
+        .default(false)
     argParser.parse(args)
 
     val sourceCode = Files.readString(Paths.get(input))
 
     val tokenizer = Tokenizer(sourceCode)
     tokenizer.run()
+    val astBuilder = AstBuilder(tokenizer.tokens)
+    astBuilder.build()
 
-    val asmGenerator = AsmGenerator(tokenizer.tokens)
-    val generatedAsm = asmGenerator.generate()
-    if(printAsm) println(generatedAsm)
+    if (debug) {
+        tokenizer.tokens.forEach { println(it) }
+        astBuilder.ast.forEach { println(it) }
+        return
+    }
 
-    Files.write(Paths.get(output ?: "${input.split(".")[0]}.asm"), generatedAsm.toByteArray())
+    //val asmGenerator = AsmGenerator(astBuilder.ast)
+    //val generatedAsm = asmGenerator.generate()
+    //if(printAsm) println(generatedAsm)
+
+    //Files.write(Paths.get(output ?: "${input.split(".")[0]}.asm"), generatedAsm.toByteArray())
 }
